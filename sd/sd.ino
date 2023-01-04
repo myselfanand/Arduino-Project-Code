0b444e638d66758d0ac306a74c09dd660a465f00
myselfanand <121082777+myselfanand@users.noreply.github.com>
2023-01-04 16:01:57 +0530

    Add files via upload
    Save data in SD card using ESP32

---
+++ sd/sd.ino
@@ -0,0 +1,88 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+
+void setup(){
+    Serial.begin(115200);
+    if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+//    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+//    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+//    writeFile(SD, "/biii_tech.txt", "Welcome to ");
+//    appendFile(SD, "/biii_tech.txt", "BiiiTech!\n");
+     readFile(SD, "/biii_tech.txt");
+}
+
+void loop(){
+
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
