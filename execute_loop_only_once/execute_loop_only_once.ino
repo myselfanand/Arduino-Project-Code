0ec847a98f3822f4ed3c993c2c6dd9981adfd728
myselfanand <121082777+myselfanand@users.noreply.github.com>
2023-01-06 18:08:11 +0530

    Update execute_loop_only_once.ino

--- execute_loop_only_once/execute_loop_only_once.ino
+++ execute_loop_only_once/execute_loop_only_once.ino
@@ -1,3 +1,4 @@
+// methode 1 to execute loop once
 int i = 0;
 
 void setup() {
@@ -10,3 +11,21 @@ void loop() {
     i++;
   }
 }
+
+/*
+// methode 2 to execute loop once
+bool control_loop; // by default it is false; it u want u declare them, then it is as:- bool control_loop = false;
+//bool control_loop = false;
+
+void setup() {
+  Serial.begin(115200);   // This initializes the Serial monitor
+}
+
+void loop() {
+ if(!control_loop) 
+  {
+    Serial.print("hello, world!");  // Display on Serial monitor
+    control_loop = true;
+  }
+}
+*/
